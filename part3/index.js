d933631ee8f8deb50048eb518c6b46a2fee85410
Babajide Oluwaferanmi <babajideoluwaferanmi301@gmail.com>
2024-10-25 21:21:37 +0100

    Validation and deployment

--- part3/index.js
+++ part3/index.js
@@ -63,24 +63,23 @@ app.delete('/api/notes/:id', (req, res, next) => {
 // For editing existing notes
 
 app.put('/api/notes/:id', (req, res, next) => {
-  const body = request.body;
+  const { content, important } = request.body;
 
-  const note = {
-    content: body.content,
-    important: body.important,
-  };
-
-  Note.findByIdAndUpdate(req.params.id, note, { new: true })
+  Note.findByIdAndUpdate(
+    req.params.id,
+    { content, important },
+    { new: true, runValidator: true, context: 'query' }
+  )
     .then((updatedNote) => {
       res.json(updatedNote);
     })
     .catch((error) => next(error));
 });
 
-app.post('/api/notes', (req, res) => {
+app.post('/api/notes', (req, res, next) => {
   const body = req.body;
 
-  if (!body.content) {
+  if (body.content === undefined) {
     return res.status(400).json({ error: 'content missing' });
   }
 
@@ -89,12 +88,13 @@ app.post('/api/notes', (req, res) => {
     important: body.important || false,
   });
 
-  note.save().then((savedNote) => {
-    res.json(savedNote);
-  });
+  note
+    .save()
+    .then((savedNote) => {
+      res.json(savedNote);
+    })
+    .catch((error) => next(error));
 });
-
-//
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' });
@@ -107,6 +107,8 @@ const errorHandler = (error, req, res, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' });
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message });
   }
   next(error);
 };
