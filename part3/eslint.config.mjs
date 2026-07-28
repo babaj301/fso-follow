3a9838122949ac7563d8217061d49ce780a86da7
Babajide Oluwaferanmi <babajideoluwaferanmi301@gmail.com>
2026-07-28 22:59:20 +0100

    fix: edit the eslint errors

--- part3/eslint.config.mjs
+++ part3/eslint.config.mjs
@@ -20,8 +20,6 @@ export default [
     rules: {
       '@stylistic/js/indent': ['error', 2],
       '@stylistic/js/linebreak-style': ['error', 'unix'],
-      '@stylistic/js/quotes': ['error', 'single'],
-      '@stylistic/js/semi': ['error', 'never'],
       eqeqeq: 'error',
       'no-trailing-spaces': 'error',
       'object-curly-spacing': ['error', 'always'],
